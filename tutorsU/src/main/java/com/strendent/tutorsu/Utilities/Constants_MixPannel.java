ce541cf15143fa14c7619b729dfe4a2d69c649d7
NoumanGhaffar1 <nouman.ghaffar@strendent.com>
2015-07-03 03:53:41 +0500

    New Code

---
+++ tutorsU/src/main/java/com/strendent/tutorsu/Utilities/Constants_MixPannel.java
@@ -0,0 +1,25 @@
+package com.strendent.tutorsu.Utilities;
+
+/**
+ * Created by user on 7/3/2015.
+ */
+public class Constants_MixPannel {
+
+
+       /* try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.strendent.tutorsu",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }*/
+
+
+}
